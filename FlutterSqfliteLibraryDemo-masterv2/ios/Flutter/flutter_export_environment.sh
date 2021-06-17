6f4945a181e1d01892f383dda234c59acca7059f
davquev <ingdavquev@gmail.com>
2021-06-16 21:51:55 -0500

    Py ShoppingList completo

--- FlutterSqfliteLibraryDemo-masterv2/ios/Flutter/flutter_export_environment.sh
+++ FlutterSqfliteLibraryDemo-masterv2/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src3\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Public\dav_d\movilesWX71_2101\FlutterSqfliteLibraryDemo-master"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Public\dav_d\movilesWX71_2101\FlutterSqfliteLibraryDemo-masterv2"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
